a7c39eb96fdc8b1b36009192b7e0f225a056a241
Alexander Kormanovsky <kormanowskij@yandex.ru>
2022-10-31 00:59:57 +0300

    implement autosaves

--- control.lua
+++ control.lua
@@ -1,6 +1,18 @@
---https://forums.factorio.com/viewtopic.php?p=200995#p200995 
+--https://forums.factorio.com/viewtopic.php?p=200995#p200995
+--http://lua-api.hornwitser.no/1.1.49/LuaGameScript.html#LuaGameScript.remove_path
 
 --Handles hot key
 script.on_event("perform-quicksave", function(event)
-  game.auto_save("TEST")
+  local max_saves_number = 3
+  local last_save_number = global["last_save_number"]
+
+  if (last_save_number == nil or last_save_number >= max_saves_number) then
+    last_save_number = 0
+  end
+
+  last_save_number = last_save_number + 1
+
+  global["last_save_number"] = last_save_number
+
+  game.auto_save(string.format("quick%d", last_save_number))
 end)
